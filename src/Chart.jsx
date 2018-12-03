46221bfa7f30b26bb8dd582329d657840fefe803
Garrett Kirschbaum <garrett@podium.com>
2018-12-03 00:16:53 -0700

    added prettier

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import * as colors from './colors';
 import Rectangle from './Rectangle';
 import ChartWrapper from './ChartStyledComponents';
-import { detectChartType, getStackPositions, singleLineChart } from './chartHelpers'
+import {
+  detectChartType,
+  getStackPositions,
+  singleLineChart
+} from './chartHelpers';
 import {
   Bar as RechartsBar,
   CartesianGrid as RechartsCartesianGrid,
@@ -21,15 +25,15 @@ export default class Chart extends React.Component {
     this.singleLineChart = singleLineChart(props.children);
   }
 
-  renderChildren = (mapping) => {
+  renderChildren = mapping => {
     const { children } = this.props;
     return React.Children.map(children, child => {
       const renderComponent = mapping[child.type.name];
-      if (renderComponent) return renderComponent(child.props)
+      if (renderComponent) return renderComponent(child.props);
     });
   };
 
-  renderXAxis = (props) => (
+  renderXAxis = props => (
     <RechartsXAxis
       axisLine={false}
       tickLine={false}
@@ -37,9 +41,9 @@ export default class Chart extends React.Component {
       fontSize={11}
       {...props}
     />
-  )
+  );
 
-  renderYAxis = (props) => (
+  renderYAxis = props => (
     <RechartsYAxis
       stroke="#ADB6BE"
       axisLine={false}
@@ -48,47 +52,52 @@ export default class Chart extends React.Component {
       fontSize={11}
       {...props}
     />
-  )
+  );
 
-  renderBar = (props) => (
+  renderBar = props => (
     <RechartsBar
       shape={<Rectangle {...props} stackPosition={this.stackPosition} />}
       fill={props.color}
       {...props}
     />
-  )
+  );
 
-  renderLine = (props) => (
+  renderLine = props => (
     <RechartsLine
       type="linear"
       stroke={props.color}
       isAnimationActive={false}
       strokeWidth={2}
       activeDot={false}
-      dot={{strokeWidth: 0, fill: props.color }}
+      dot={{ strokeWidth: 0, fill: props.color }}
       {...props}
     />
-  )
+  );
 
-  renderTooltip = (props) => {
-    let cursorSettings = { fill: '#F1F2F4', strokeWidth: 1 }
+  renderTooltip = props => {
+    let cursorSettings = { fill: '#F1F2F4', strokeWidth: 1 };
     if (this.singleLineChart) {
-      cursorSettings = { ...cursorSettings, stroke: this.singleLineChart.color }
+      cursorSettings = {
+        ...cursorSettings,
+        stroke: this.singleLineChart.color
+      };
     }
-    return <RechartsTooltip
-      cursor={cursorSettings}
-      isAnimationActive={false}
-      offset={20}
-      wrapperStyle={{
-        minWidth: '160px',
-        borderRadius: '4px',
-        backgroundColor: colors.white,
-        boxShadow: '0 5px 12px 0 rgba(0,0,0,0.24)',
-        padding: '16px 24px'
-      }}
-      {...props}
-    />
-  }
+    return (
+      <RechartsTooltip
+        cursor={cursorSettings}
+        isAnimationActive={false}
+        offset={20}
+        wrapperStyle={{
+          minWidth: '160px',
+          borderRadius: '4px',
+          backgroundColor: colors.white,
+          boxShadow: '0 5px 12px 0 rgba(0,0,0,0.24)',
+          padding: '16px 24px'
+        }}
+        {...props}
+      />
+    );
+  };
 
   render() {
     const { data, width, height } = this.props;
@@ -99,7 +108,7 @@ export default class Chart extends React.Component {
       Bar: this.renderBar,
       Line: this.renderLine,
       Tooltip: this.renderTooltip
-    }
+    };
 
     return (
       <ChartWrapper>
@@ -121,10 +130,10 @@ export default class Chart extends React.Component {
 Chart.propTypes = {
   data: PropTypes.array.isRequired,
   width: PropTypes.number,
-  height: PropTypes.number,
+  height: PropTypes.number
 };
 
 Chart.defaultProps = {
   width: 730,
-  height: 250,
+  height: 250
 };
