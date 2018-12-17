f84c6d298cf98cc3a458cab7d5d0d1b2ba25241e
Tyler <backdoer@gmail.com>
2018-12-17 14:51:43 -0700

    title case

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -26,7 +26,13 @@ const Space = styled.div`
   padding: 8px;
 `;
 
-export default function Summary({ data, dataKeys, summaryType, formatter }) {
+export default function Summary({
+  data,
+  dataKeys,
+  summaryType,
+  formatter,
+  granularity
+}) {
   const typeHandler = {
     total: monthData =>
       dataKeys.reduce((acc, key) => (monthData[key] || 0) + acc, 0),
@@ -56,20 +62,19 @@ export default function Summary({ data, dataKeys, summaryType, formatter }) {
     return entireDataTypeHandler[summaryType](data);
   };
 
-  const granularity = () => {
-    const granInMili = moment(data[1].date).diff(moment(data[0].date));
-    const duration = moment.duration(granInMili);
-    if (duration.years()) return 'Year';
-    if (duration.months()) return 'Month';
-    if (duration.weeks()) return 'Week';
-    if (duration.days()) return 'Day';
-    if (duration.hours()) return 'Hour';
-    return '';
+  const titleCase = str => {
+    return str
+      .toLowerCase()
+      .split(' ')
+      .map(function(word) {
+        return word.replace(word[0], word[0].toUpperCase());
+      })
+      .join(' ');
   };
 
   return (
     <SummaryWrapper>
-      <ToDate>{granularity()} to Date</ToDate>
+      <ToDate>{titleCase(granularity)} to Date</ToDate>
       <SummaryLabel>{formatter(currentData())}</SummaryLabel>
       <Space />
       <Last12Months>Last 12 Months</Last12Months>
