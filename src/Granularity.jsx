813e525276c44e610545e9be33eb8df6ea29d3a7
NickHath <Nicholashathaway@gmail.com>
2019-01-08 09:28:16 -0700

    remove valid gran check

--- src/Granularity.jsx
+++ src/Granularity.jsx
@@ -64,9 +64,9 @@ export default class Granularity extends Component {
     const options = this.getOptions();
     const placeholder = options[0].label || '';
 
-    const validRangeValues = options.map(option => option.value);
-    if (!(value in validRangeValues) && value !== 'custom')
-      onChange(validRangeValues[0]);
+    // const validRangeValues = options.map(option => option.value);
+    // if (!(value in validRangeValues) && value !== 'custom')
+    //   onChange(validRangeValues[0]);
 
     return (
       <GranularityWrapper>
