1dd837ede10948c48b12f1c2e289cc0f69218192
NickHath <Nicholashathaway@gmail.com>
2019-01-08 16:42:43 -0700

    rerun build

--- build/Granularity.js
+++ build/Granularity.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _includes2 = _interopRequireDefault(require("lodash/includes"));
+
 var _react = _interopRequireWildcard(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
@@ -15,9 +17,9 @@ var _styledComponents = _interopRequireDefault(require("styled-components"));
 
 var _moment = _interopRequireDefault(require("moment"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
@@ -138,8 +140,7 @@ function (_Component) {
         var validRangeValues = options.map(function (option) {
           return option.value;
         });
-        console.log(validRangeValues);
-        if (!(value in validRangeValues) && value !== 'custom') onChange(validRangeValues[0]);
+        if (!(0, _includes2.default)(validRangeValues, value)) onChange(validRangeValues[0]);
       }
     }, _temp));
   }
