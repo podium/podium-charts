4710cbf88408831c9d906b9548e92afa185a0e18
Tyler Doermann <backdoer@gmail.com>
2019-01-18 11:45:07 -0700

    dependencies added

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+
+'use strict';
+
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: ['transform-class-properties']
+};
