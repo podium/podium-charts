8fe24b24db3b6fffbf00805e48350901499575db
Andrew Jensen <andrewjensen90@gmail.com>
2019-02-05 13:25:39 -0700

    Run the build

--- build/Legend.js
+++ build/Legend.js
@@ -9,7 +9,7 @@ var _react = _interopRequireDefault(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _styledComponents = _interopRequireDefault(require("styled-components"));
+var _styledComponents = _interopRequireWildcard(require("styled-components"));
 
 var _podiumUi = require("@podiumhq/podium-ui");
 
@@ -21,14 +21,28 @@ var _formatters = _interopRequireDefault(require("./formatters"));
 
 var _lodash = _interopRequireDefault(require("lodash.get"));
 
+var _ReportCardContext = _interopRequireDefault(require("./ReportCardContext"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function _templateObject4() {
+function _templateObject5() {
   var data = _taggedTemplateLiteral(["\n  display: flex;\n  align-items: center;\n"]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject4() {
+  var data = _taggedTemplateLiteral(["\n  width: 16px;\n  height: 16px;\n  border-radius: 2px;\n  background-color: ", ";\n  margin-right: 8px;\n"]);
 
   _templateObject4 = function _templateObject4() {
     return data;
@@ -38,7 +52,7 @@ function _templateObject4() {
 }
 
 function _templateObject3() {
-  var data = _taggedTemplateLiteral(["\n  width: 16px;\n  height: 16px;\n  border-radius: 2px;\n  background-color: ", ";\n  margin-right: 8px;\n"]);
+  var data = _taggedTemplateLiteral(["\n      opacity: 0.3;\n    "]);
 
   _templateObject3 = function _templateObject3() {
     return data;
@@ -48,7 +62,7 @@ function _templateObject3() {
 }
 
 function _templateObject2() {
-  var data = _taggedTemplateLiteral(["\n  width: 100%;\n  display: flex;\n  align-items: center;\n  justify-content: space-between;\n  margin-top: 8px;\n"]);
+  var data = _taggedTemplateLiteral(["\n  width: 100%;\n  display: flex;\n  align-items: center;\n  justify-content: space-between;\n  padding-top: 4px;\n  padding-bottom: 4px;\n  cursor: default;\n\n  ", "\n"]);
 
   _templateObject2 = function _templateObject2() {
     return data;
@@ -58,7 +72,7 @@ function _templateObject2() {
 }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  padding-top: 8px;\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  color: ", ";\n  text-align: left;\n  justify-content: center;\n  font-size: 12px;\n"]);
+  var data = _taggedTemplateLiteral(["\n  padding-top: 12px;\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  color: ", ";\n  text-align: left;\n  justify-content: center;\n  font-size: 12px;\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -71,13 +85,15 @@ function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(
 
 var LegendWrapper = _styledComponents.default.div(_templateObject(), _podiumUi.colors.mineShaft);
 
-var ItemWrapper = _styledComponents.default.div(_templateObject2());
+var ItemWrapper = _styledComponents.default.div(_templateObject2(), function (props) {
+  return !props.enabled && (0, _styledComponents.css)(_templateObject3());
+});
 
-var ColorLabel = _styledComponents.default.div(_templateObject3(), function (props) {
+var ColorLabel = _styledComponents.default.div(_templateObject4(), function (props) {
   return props.color;
 });
 
-var Label = _styledComponents.default.div(_templateObject4());
+var Label = _styledComponents.default.div(_templateObject5());
 
 function Legend(_ref) {
   var loading = _ref.loading,
@@ -114,13 +130,24 @@ function Legend(_ref) {
 
   var renderLegendItems = function renderLegendItems() {
     var aggMap = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-    return displayOptions.reverse().map(function (legendItem) {
+    var selectedKey = arguments.length > 1 ? arguments[1] : undefined;
+    var onSelectKey = arguments.length > 2 ? arguments[2] : undefined;
+    // TODO: why do we need to reverse the displayOptions?
+    // Maybe we want to reverse the rendering of stacked bars instead
+    return displayOptions.slice().reverse().map(function (legendItem) {
       var dataKey = legendItem.dataKey,
           color = legendItem.color,
           name = legendItem.name;
       var formattedValue = aggMap[dataKey] && formatter(aggMap[dataKey]);
       return _react.default.createElement(ItemWrapper, {
-        key: name
+        key: name,
+        enabled: !selectedKey || dataKey === selectedKey,
+        onMouseEnter: function onMouseEnter() {
+          return onSelectKey(dataKey);
+        },
+        onMouseLeave: function onMouseLeave() {
+          return onSelectKey(null);
+        }
       }, _react.default.createElement(Label, null, _react.default.createElement(ColorLabel, {
         color: color
       }), _react.default.createElement("div", null, name ? name : '')), formattedValue && _react.default.createElement("div", null, formattedValue));
@@ -130,7 +157,11 @@ function Legend(_ref) {
   if (loading) return renderGhostState();
   var dataKeys = (0, _lodash.default)(aggregationOptions, 'dataKeys');
   var aggMap = createAggMap(dataKeys);
-  return _react.default.createElement(LegendWrapper, null, renderLegendItems(aggMap));
+  return _react.default.createElement(_ReportCardContext.default.Consumer, null, function (_ref2) {
+    var selectedKey = _ref2.selectedKey,
+        onSelectKey = _ref2.onSelectKey;
+    return _react.default.createElement(LegendWrapper, null, renderLegendItems(aggMap, selectedKey, onSelectKey));
+  });
 }
 
 Legend.propTypes = {
--- build/ReportCard.js
+++ build/ReportCard.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = ReportCard;
+exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -27,11 +27,31 @@ var _Legend = _interopRequireDefault(require("./Legend"));
 
 var _GhostChart = _interopRequireDefault(require("./Ghost/GhostChart"));
 
+var _ReportCardContext = _interopRequireDefault(require("./ReportCardContext"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 function _templateObject6() {
   var data = _taggedTemplateLiteral(["\n  height: 100%;\n  padding: 16px 24px 16px 24px;\n"]);
@@ -120,47 +140,81 @@ var defaultComponents = {
   ghost: null
 };
 
-function ReportCard(_ref2) {
-  var width = _ref2.width,
-      children = _ref2.children,
-      loading = _ref2.loading;
+var ReportCard =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(ReportCard, _React$Component);
 
-  var collectChildren = function collectChildren() {
-    if (!children) return _objectSpread({}, defaultComponents);
+  function ReportCard(props) {
+    var _this;
 
-    var newComponents = _objectSpread({}, defaultComponents);
+    _classCallCheck(this, ReportCard);
 
-    _react.default.Children.forEach(children, function (child) {
-      if (componentKeyMap.has(child.type)) {
-        newComponents[componentKeyMap.get(child.type)] = _react.default.cloneElement(child, {
-          loading: loading
-        });
-      } else if (child.props.children) {
-        _react.default.Children.forEach(child.props.children, function (subChild) {
-          if (componentKeyMap.has(subChild.type)) {
-            newComponents[componentKeyMap.get(subChild.type)] = _react.default.cloneElement(child, {
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(ReportCard).call(this, props));
+
+    _this.onSelectKey = function (dataKey) {
+      _this.setState({
+        selectedKey: dataKey
+      });
+    };
+
+    _this.state = {
+      selectedKey: null,
+      onSelectKey: _this.onSelectKey
+    };
+    return _this;
+  }
+
+  _createClass(ReportCard, [{
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          children = _this$props.children,
+          loading = _this$props.loading;
+
+      var collectChildren = function collectChildren() {
+        if (!children) return _objectSpread({}, defaultComponents);
+
+        var newComponents = _objectSpread({}, defaultComponents);
+
+        _react.default.Children.forEach(children, function (child) {
+          if (componentKeyMap.has(child.type)) {
+            newComponents[componentKeyMap.get(child.type)] = _react.default.cloneElement(child, {
               loading: loading
+            });
+          } else if (child.props.children) {
+            _react.default.Children.forEach(child.props.children, function (subChild) {
+              if (componentKeyMap.has(subChild.type)) {
+                newComponents[componentKeyMap.get(subChild.type)] = _react.default.cloneElement(child, {
+                  loading: loading
+                });
+              }
             });
           }
         });
-      }
-    });
 
-    return newComponents;
-  };
+        return newComponents;
+      };
 
-  var _collectChildren = collectChildren(),
-      title = _collectChildren.title,
-      chart = _collectChildren.chart,
-      summary = _collectChildren.summary,
-      legend = _collectChildren.legend,
-      granularity = _collectChildren.granularity;
+      var _collectChildren = collectChildren(),
+          title = _collectChildren.title,
+          chart = _collectChildren.chart,
+          summary = _collectChildren.summary,
+          legend = _collectChildren.legend,
+          granularity = _collectChildren.granularity;
 
-  return _react.default.createElement(ReportCardWrapper, null, _react.default.createElement(ReportCardMain, {
-    fullWidth: !summary && !legend
-  }, _react.default.createElement(ReportCardHeader, null, title, " ", granularity), chart), (summary || legend) && _react.default.createElement(ReportCardRight, null, _react.default.createElement(ReportCardSummary, null, _react.default.createElement(Padding, null, summary, " ", legend))));
-}
+      return _react.default.createElement(_ReportCardContext.default.Provider, {
+        value: this.state
+      }, _react.default.createElement(ReportCardWrapper, null, _react.default.createElement(ReportCardMain, {
+        fullWidth: !summary && !legend
+      }, _react.default.createElement(ReportCardHeader, null, title, " ", granularity), chart), (summary || legend) && _react.default.createElement(ReportCardRight, null, _react.default.createElement(ReportCardSummary, null, _react.default.createElement(Padding, null, summary, " ", legend)))));
+    }
+  }]);
 
+  return ReportCard;
+}(_react.default.Component);
+
+exports.default = ReportCard;
 ReportCard.propTypes = {
   children: _propTypes.default.array,
   loading: _propTypes.default.bool
