9ebc40f8c83f52287840a195e05444d1938600a7
Brian Bolnick <brianbolnick@gmail.com>
2019-04-08 13:30:56 -0600

    add new file for testing

---
+++ build/Stories/Test.story.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import SearchBar from '../SearchBar';
+import { DefaultNotes } from './SearchBarHelpers';
+storiesOf('Test', module).add('default', function () {
+  return React.createElement("div", {
+    style: {
+      width: '400px'
+    }
+  }, React.createElement(SearchBar, {
+    onChange: function onChange(e) {
+      return console.log(e.target.value);
+    }
+  }));
+}, {
+  notes: DefaultNotes
+});
