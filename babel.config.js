f46600f42c5756546bf6f3461e366cab5affdee0
NickHath <Nicholashathaway@gmail.com>
2019-04-10 10:07:19 -0600

    merge with fixed tests branch

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,24 @@
 
 'use strict';
 
-module.exports = {
+const TEST_CONFIG = {
+  presets: [
+    ['@babel/preset-env',],
+    '@babel/preset-react'
+  ],
+  plugins: ['transform-class-properties']
+}
+
+const CONFIG = {
   presets: [
     ['@babel/preset-env', { modules: false }],
     '@babel/preset-react'
   ],
-  plugins: ['transform-class-properties']
+  plugins: ['transform-class-properties'],
+  ignore: ['./src/__tests__']
+}
+
+module.exports = api => {
+  const isTest = api.env('test');
+  return isTest ? TEST_CONFIG : CONFIG;
 };
