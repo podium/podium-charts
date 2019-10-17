0182c1800c5d42d96111ac320bc7334163ba461c
Brian Bolnick <brianbolnick@gmail.com>
2019-10-17 11:06:50 -0600

    Fix currency nulls (#126)
    * change default tooltip positioning on table
    
    * fix null issue on currency formatter
    
    * bump version

--- build/Colors/index.js
+++ build/Colors/index.js
@@ -1,29 +1,19 @@
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 import { colors as podiumUiColors } from '@podiumhq/podium-ui';
-var podiumBrand = podiumUiColors.podiumBrand,
-    goldfish = podiumUiColors.goldfish,
-    cobaltBlue = podiumUiColors.cobaltBlue,
-    siteColors = podiumUiColors.siteColors,
-    poppyRed = podiumUiColors.poppyRed,
-    mystic = podiumUiColors.mystic,
-    white = podiumUiColors.white,
-    steel = podiumUiColors.steel;
-var colors = {
+
+var colors = _objectSpread({}, podiumUiColors, {
   athensGray: '#e8e9ec',
   bondiBlue: '#008DB8',
   chathamsBlue: '#11497D',
-  cobaltBlue: cobaltBlue,
-  goldfish: goldfish,
   hawkesBlue: '#CCD7FC',
   jollyGreen: '#00804F',
   mandy: '#EE5866',
   mariner: '#3768D3',
   matisse: '#1B5493',
-  mystic: mystic,
-  podiumBrand: podiumBrand,
-  poppyRed: poppyRed,
-  siteColors: siteColors,
-  steel: steel,
-  turquoise: '#28CBAD',
-  white: white
-};
+  turquoise: '#28CBAD'
+});
+
 export default colors;
