54f83a211f746d92c6456980385bfee0575c2597
TaylorWu21 <Taylorwu21@gmail.com>
2019-10-17 11:03:27 -0600

    export podium colors (#125)

--- src/Colors/index.js
+++ src/Colors/index.js
@@ -1,33 +1,16 @@
 import { colors as podiumUiColors } from '@podiumhq/podium-ui';
-const {
-  podiumBrand,
-  goldfish,
-  cobaltBlue,
-  siteColors,
-  poppyRed,
-  mystic,
-  white,
-  steel
-} = podiumUiColors;
 
 const colors = {
+  ...podiumUiColors,
   athensGray: '#e8e9ec',
   bondiBlue: '#008DB8',
   chathamsBlue: '#11497D',
-  cobaltBlue,
-  goldfish,
   hawkesBlue: '#CCD7FC',
   jollyGreen: '#00804F',
   mandy: '#EE5866',
   mariner: '#3768D3',
   matisse: '#1B5493',
-  mystic,
-  podiumBrand,
-  poppyRed,
-  siteColors,
-  steel,
   turquoise: '#28CBAD',
-  white
 };
 
 export default colors;
